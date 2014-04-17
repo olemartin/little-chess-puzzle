a3c6283a6f59c9b552d74c502cd4f945ef330331
Ole-Martin Mørk <olemartin@gmail.com>
2014-04-18 00:54:52 +0200

    Added redirect from root

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get('/edition', function(req, res) {
 	});
 });
 
+app.get('/', function(req, res) {
+    res.redirect('/edition');
+})
+
 var server = app.listen(process.env.PORT || 3000, function() {
 	console.log('Listening on port %d', server.address().port);
 });
