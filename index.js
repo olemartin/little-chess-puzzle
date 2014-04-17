9dc849865a4ff4d19443b6a214571bea8dc0a9e0
Ole-Martin Mørk <olemartin@gmail.com>
2014-04-18 00:52:26 +0200

    Added heroku port

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/edition', function(req, res) {
 	});
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 	console.log('Listening on port %d', server.address().port);
 });
