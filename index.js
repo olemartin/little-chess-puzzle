37b2a6923ac3bf79dca87833dab00087b2d6bb2d
Ole-Martin Mørk <olemartin@gmail.com>
2014-04-18 12:12:24 +0200

    Refactored, created sample that is equal all the time

--- index.js
+++ index.js
@@ -9,25 +9,31 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(require('etagify')());
 
-
-var fetchData = function(req, res) {
-    lichess.fetch(function(response) {
-        res.etagify();
-        res.render('index', {
-            board: chessboard.generateBoard(response.position),
-            solution : response.solution
-        });
+var render = function (res, response) {
+    res.etagify();
+    res.render('index', {
+        board: chessboard.generateBoard(response.position),
+        solution: response.solution
     });
 };
 
-app.get('/edition', fetchData);
-app.get('/sample', fetchData);
+app.get('/edition', function (req, res) {
+    lichess.fetch(function (response) {
+        render(res, response)
+    });
+});
+
+app.get('/sample', function (req, res) {
+    lichess.sample(function (response) {
+        render(res, response)
+    });
+});
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.redirect('/edition');
 });
 
-var server = app.listen(process.env.PORT || 3000, function() {
-	console.log('Listening on port %d', server.address().port);
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port %d', server.address().port);
 });
