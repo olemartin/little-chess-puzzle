050d3b3414d677147ed0f3cd34a8b6a9651888af
Ole-Martin Mørk <olemartin@gmail.com>
2014-04-18 11:25:56 +0200

    Some refactorings

--- index.js
+++ index.js
@@ -8,27 +8,23 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/edition', function(req, res) {	
-	lichess.fetch(function(response) {
-		res.render('index', {
-			board: chessboard.generateBoard(response.position),
-            solution : response.solution
-		});
-	});
-});
 
-app.get('/sample', function(req, res) {
+var fetchData = function(req, res) {
     lichess.fetch(function(response) {
         res.render('index', {
             board: chessboard.generateBoard(response.position),
             solution : response.solution
         });
     });
-});
+};
+
+app.get('/edition', fetchData);
+app.get('/sample', fetchData);
+
 
 app.get('/', function(req, res) {
     res.redirect('/edition');
-})
+});
 
 var server = app.listen(process.env.PORT || 3000, function() {
 	console.log('Listening on port %d', server.address().port);
